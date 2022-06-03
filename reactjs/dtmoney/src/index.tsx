eb521acd658ed45f2d10282aa062cd6947ce936d
Bruno Duarte <brunosduarte@hotmail.com>
2022-06-03 09:38:26 -0300

    sending data to api

--- reactjs/dtmoney/src/index.tsx
+++ reactjs/dtmoney/src/index.tsx
@@ -20,6 +20,12 @@ createServer({
       ]
     })
 
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+      return data
+
+    })
+
   }
 })
 
