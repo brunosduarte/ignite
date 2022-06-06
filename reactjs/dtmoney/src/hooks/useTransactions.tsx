a999ea62dcd6864a89c8a43e56ccafa4fe17d986
Bruno Duarte <brunosduarte@hotmail.com>
2022-06-06 14:20:51 -0300

    ig.news layout

--- reactjs/dtmoney/src/hooks/useTransactions.tsx
+++ reactjs/dtmoney/src/hooks/useTransactions.tsx
@@ -1,5 +1,5 @@
-import { createContext, ReactNode, useEffect, useState } from 'react';
-import { api } from './services/api';
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+import { api } from '../services/api';
 
 interface Transaction {
     id: number;
@@ -21,7 +21,7 @@ interface TransactionsContextData {
     createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
-export const TransactionsContext = createContext<TransactionsContextData>(
+const TransactionsContext = createContext<TransactionsContextData>(
     {} as TransactionsContextData
 );
 
@@ -52,4 +52,9 @@ export function TransactionsProvider( { children }: TransactionsProviderProps) {
         </TransactionsContext.Provider>
     )
 
+}
+
+export function useTransactions() {
+    const context = useContext(TransactionsContext);
+    return context;
 }
