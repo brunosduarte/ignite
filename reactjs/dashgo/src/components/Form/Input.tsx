3d01fdb5d5cdd63079ad9247f536163e2fbfbaa4
Bruno Duarte <brunosduarte@hotmail.com>
2022-06-15 11:29:50 -0300

    dashgo initial

---
+++ reactjs/dashgo/src/components/Form/Input.tsx
@@ -0,0 +1,28 @@
+import { FormLabel, FormControl, Input as ChakraInput, InputProps as ChakraInputProps } from '@chakra-ui/react';
+
+interface InputProps extends ChakraInputProps{
+    name: string;
+    label?: string;
+} 
+
+export function Input({ name, label, ...rest }: InputProps){
+    return (
+        <FormControl>
+           { !!label && <FormLabel htmlFor={name}>{label}</FormLabel> }
+
+            <ChakraInput
+                id={name} 
+                name={name}
+                type={name}
+                focusBorderColor="pink.500"
+                bgColor="gray.900"
+                variant="filled"
+                _hover={{
+                    bgColor: "gray.900"
+                }}
+                size="lg"
+                {...rest}
+            />
+        </FormControl>
+    )
+}
