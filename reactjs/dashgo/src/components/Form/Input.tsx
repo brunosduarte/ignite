5153710e7d759c6e90bf11f8d559fa1fd11ca7b0
Bruno Duarte <brunosduarte@hotmail.com>
2022-06-17 16:31:38 -0300

    dashgo forms with validation

--- reactjs/dashgo/src/components/Form/Input.tsx
+++ reactjs/dashgo/src/components/Form/Input.tsx
@@ -1,19 +1,22 @@
-import { FormLabel, FormControl, Input as ChakraInput, InputProps as ChakraInputProps } from '@chakra-ui/react';
+import { forwardRef, ForwardRefRenderFunction } from 'react';
+import { FieldError } from 'react-hook-form';
+import { FormLabel, FormControl, Input as ChakraInput, InputProps as ChakraInputProps, FormErrorMessage } from '@chakra-ui/react';
 
 interface InputProps extends ChakraInputProps{
     name: string;
     label?: string;
+    error?: FieldError;
 } 
 
-export function Input({ name, label, ...rest }: InputProps){
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> 
+    = ({ name, label, error = null, ...rest }, ref) => {
     return (
-        <FormControl>
+        <FormControl isInvalid={!!error}>
            { !!label && <FormLabel htmlFor={name}>{label}</FormLabel> }
 
             <ChakraInput
                 id={name} 
                 name={name}
-                type={name}
                 focusBorderColor="pink.500"
                 bgColor="gray.900"
                 variant="filled"
@@ -21,8 +24,17 @@ export function Input({ name, label, ...rest }: InputProps){
                     bgColor: "gray.900"
                 }}
                 size="lg"
+                ref={ref}
                 {...rest}
             />
+
+            { !!error && (
+                <FormErrorMessage>
+                    {error.message}
+                </FormErrorMessage>
+            )}
         </FormControl>
-    )
+    );
 }
+
+export const Input = forwardRef(InputBase);
