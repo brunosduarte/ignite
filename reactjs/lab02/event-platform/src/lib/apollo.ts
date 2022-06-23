22d1193be4c02c13bff1128f913a17d72330f504
Bruno Duarte <brunosduarte@hotmail.com>
2022-06-23 14:24:54 -0300

    iglab02 lesson03

--- reactjs/lab02/event-platform/src/lib/apollo.ts
+++ reactjs/lab02/event-platform/src/lib/apollo.ts
@@ -1,6 +1,18 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, DefaultOptions, InMemoryCache } from "@apollo/client";
+
+const defaultOptions: DefaultOptions = {
+    watchQuery: {
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'ignore',
+    },
+    query: {
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'all',
+    },
+}
 
 export const client = new ApolloClient({
-    uri: 'https://api-us-west-2.graphcms.com/v2/cl4q69wwf32di01ywb7ah3ukb/master',
-    cache: new InMemoryCache()
-});
+    uri: 'https://api-us-west-2.graphcms.com/v2/cl4q56p1c2xsy01xsgjubcc83/master',
+    cache: new InMemoryCache(),
+    defaultOptions: defaultOptions,
+})
