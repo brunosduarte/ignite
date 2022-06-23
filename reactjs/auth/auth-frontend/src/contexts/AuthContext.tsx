47460a686d572460231a525294133836a6e6bb4b
Bruno Duarte <brunosduarte@hotmail.com>
2022-06-23 16:54:59 -0300

    authlessons

---
+++ reactjs/auth/auth-frontend/src/contexts/AuthContext.tsx
@@ -0,0 +1,41 @@
+import { createContext, ReactNode } from "react";
+import { api } from "../services/api";
+
+type SignInCredentials = {
+    email: string;
+    password: string;
+}
+
+type AuthContextData = {
+    signIn(credentials: SignInCredentials): Promise<void>;
+    isAuthenticated: boolean;
+}
+
+type AuthProviderProps = {
+    children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextData)
+
+export function AuthProvider({ children }: AuthProviderProps) {
+    const isAuthenticated = false;
+
+    async function signIn({ email, password }: SignInCredentials) {
+        try {
+            const response = await api.post('sessions', {
+                email,
+                password,
+            })
+    
+            console.log(response.data)
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return (
+        <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
