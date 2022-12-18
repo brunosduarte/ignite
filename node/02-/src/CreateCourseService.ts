1236c71b2a4eb9204c7f0e2849ba0c99be8d0502
Bruno Duarte <brunosduarte@hotmail.com>
2022-12-18 02:17:37 -0300

    iglab03 -  finishing connections

---
+++ node/02-/src/CreateCourseService.ts
@@ -0,0 +1,20 @@
+
+/**
+ * name - string
+ * duration - number
+ * educator - string
+ */
+
+interface Course {
+  name: string;
+  duration: number;
+  educator: string;
+}
+class CreateCourseService {
+
+  execute({ name, duration, educator }: Course) {
+    console.log(name, duration, educator);
+  }
+}
+
+export default new CreateCourseService();
